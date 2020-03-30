98cc6063649b350084c11af4308ee38f9f458374
Venomicon <jangajda9@gmail.com>
2020-03-30 10:54:06 +0200

    Initial

--- src/main/java/org/covid/covid/controller/ExceptionHandlerController.java
+++ src/main/java/org/covid/covid/controller/ExceptionHandlerController.java
@@ -1,35 +1,20 @@
 package org.covid.covid.controller;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.exceptions.TemplateProcessingException;
-
-import java.net.URISyntaxException;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
     public static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerController.class);
 
-    @ExceptionHandler(URISyntaxException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ModelAndView handleURISyntaxException(URISyntaxException e) {
-        String message = "Country not found in the database";
-        LOGGER.error(message);
-        ModelAndView modelAndView = new ModelAndView("error/errorPage");
-        modelAndView.addObject("message", message);
-        return modelAndView;
-    }
-
-    @ExceptionHandler({Exception.class, TemplateProcessingException.class, SpelEvaluationException.class, NullPointerException.class})
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ModelAndView handleUnirestException(Exception e) {
+    public ModelAndView handleException(Exception e) {
         String message = "Unable to process your request";
         LOGGER.error(e.getMessage());
         ModelAndView modelAndView = new ModelAndView("error/errorPage");
